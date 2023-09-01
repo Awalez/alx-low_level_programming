44da1604229a0f4db82980a1c4c0e072fa968c4b
root <root@Don-Walez.localdomain>
2023-09-01 19:53:16 +0100

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 
 /**
- *  * _puts_recursion - Print a string
- *   * @s: string
- *    */
+ * _puts_recursion - Print a string
+ * @s: string
+ */
 
 void _puts_recursion(char *s)
-{ 
+{
 		if (*s == '\0')
 		{
 			_putchar('\n');
 		}
 		else
 		{
-			putchar(*s);
+			_putchar(*s);
 			_puts_recursion(++s);
 		}
 }
